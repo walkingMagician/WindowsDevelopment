5c347ed88bc98d36303a11867e2db14036dd5a71
Егор Шульга <egorsulga0@gmail.com>
2024-11-12 21:41:51 +0300

    проблемы с кнопками решить а так же добавить отдел редактирования

--- WinAPI/ListBox2/main.cpp
+++ WinAPI/ListBox2/main.cpp
@@ -1,9 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <cstdio>
+#include <commctrl.h>
+#include <string.h>
 #include "resource.h"
 
-CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
+
+CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "List", "Box"};
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -16,45 +19,68 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
+
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
+
+		
+
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
 		HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
 		for (int i = 0; i < sizeof(g_VALUES) / sizeof(g_VALUES[0]); i++)
 			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
-	}
-		break;
+	} break;
+
+	case WM_KEYDOWN:
+	{
+		switch (wParam)
+		{
+			
+		case VK_RETURN:
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_ADD), BM_CLICK, 0, 0); // лик по кнопке
+			break;
+
+		}break;
+
+	} break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
 		case IDC_BUTTON_ADD:
 			DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD_ITEM), hwnd, DlgProcAddItem, 0);
 			break;
+		case IDC_BUTTON_REMUVE:
+		{
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			SendMessage(hListBox, LB_DELETESTRING, i, 0);
+		}
+			break;
 
 		case IDOK: 
 		{
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST); // дескриптор нашего основного окна
 			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE]{};
-			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			CHAR sz_buffer[SIZE]{}; // массив под значение  
+			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0); // выдел€ем значение мышкой 
 			SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
-			//MessageBox(hwnd, sz_buffer, "Info", MB_OK | MB_ICONINFORMATION);
 
-			CHAR sz_message[SIZE]{};
+			CHAR sz_message[SIZE]{}; // массив под вывод сообщени€ 
 			sprintf(sz_message, "вы выбрали элимент є%i co значением \"%s\".", i, sz_buffer);
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 					
 		} break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		
-		}
-		break;
+		} break;
+
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
@@ -67,25 +93,42 @@ BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
-		break;
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD_ITEM));
+			break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD_ITEM);
-			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			CHAR OS_buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD_ITEM); // дескриптор edit control
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)OS_buffer); // получение текста 1
+			HWND hParent = GetParent(hwnd); // получение родительского окна
+			HWND hListBox = GetDlgItem(hParent, IDC_LIST); // дескриптор бокса
 
-			HWND hParent = GetParent(hwnd);
-			HWND hListBox = GetDlgItem(hParent, IDC_LIST);
+			int count = SendMessage(hListBox, LB_GETCOUNT, 0, 0); // получение кол-во элиментов 
+			BOOL flag = true; // флаг
 
-			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			for (int i = 0; i < count; i++)
+			{
+				CHAR sz_buffer[SIZE]{};
+				SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer); // получение элимента из бокса
+				if (strcmp(sz_buffer,OS_buffer) == 0)
+				{
+					flag = false;
+					break;
+				}
+			}	
+			if (flag && strlen(OS_buffer) && strlen(OS_buffer + 1)) // проверка на флаг, длину\пустоту
+				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)OS_buffer); // добавление текста
+			else
+				MessageBox(hwnd, "Ёлемент уже существует или €вл€етс€ пустым", "Error", MB_OK | MB_ICONERROR); // ошибка
+
 		} 
-		case IDCANCEL: EndDialog(hwnd, 0);
-		}
-		break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		} break;
+
 	case WM_CLOSE: EndDialog(hwnd, 0); break;
 	}
 	return false;
--- WinAPI/ListBox2/resource.h
+++ WinAPI/ListBox2/resource.h
@@ -10,6 +10,7 @@
 #define IDC_BUTTON_REMUVE               1003
 #define IDC_EDIT1                       1004
 #define IDC_EDIT_ADD_ITEM               1004
+#define IDC_LIST1                       1006
 
 // Next default values for new objects
 // 
@@ -17,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
