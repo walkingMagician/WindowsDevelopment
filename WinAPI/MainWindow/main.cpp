90fb69212385f0a40c92d5a745b0b7dde1277358
Егор Шульга <egorsulga0@gmail.com>
2024-11-18 23:02:21 +0300

    resourse - icon done

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,11 +1,17 @@
 ﻿#include<windows.h>
+#pragma comment(lib,"user32")
+#include "resource.h"
+
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Main Window";
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+int WindowSizeX();
+int WindowSizeY();
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
+
 	WNDCLASSEX wClass;
 	ZeroMemory(&wClass, sizeof(wClass));
 
@@ -16,9 +22,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wClass.cbWndExtra = 0;
 	// cb...  - CountBytes
 
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)); 
+	// Используется для отображения значка окна в заголовке окна и на панелях задач
+	wClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));
+	// предназначен для использования в заголовках окон и в списке задач
+	wClass.hCursor = LoadCursor(hInstance, IDC_ARROW);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	wClass.hInstance = hInstance;
@@ -38,8 +46,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		g_sz_WINDOW_CLASS, // Class name
 		g_sz_WINDOW_CLASS, // Window Title
 		WS_OVERLAPPEDWINDOW, // Window style. такой стиль всегда задаётся для главного окна. 
-		CW_USEDEFAULT, CW_USEDEFAULT, // position - положение окна при запуске
-		CW_USEDEFAULT, CW_USEDEFAULT, // size - размер создоваемого окнаокна 
+		CW_USEDEFAULT, CW_USEDEFAULT, // position - положение окна при запуске от левого верхнего угла (px)
+		WindowSizeX(), WindowSizeY(), // size - размер создоваемого окнаокна (px) 
 		NULL,
 		NULL, // hMenu - для главного окна это ResourceID главного меню,
 		// для дочернего окна (элимента какого-то окна)
@@ -90,4 +98,23 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return 0;
+}
+
+int WindowSizeX()
+{
+	// Получение размера экрана
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+
+	// Рассчитываем размеры окна (50% высоты и ширины)
+	int windowWidth = screenWidth / 2;
+	return windowWidth;
+}
+int WindowSizeY()
+{
+	// Получение размера экрана
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	// Рассчитываем размеры окна (50% высоты и ширины)
+	int windowHeight = screenHeight / 2;
+	return windowHeight;
 }
