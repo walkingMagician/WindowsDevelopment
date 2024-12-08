57db608a286f7033bd58feca0db04c2ded1f781f
I'm kotэ <egorsulga0@gmail.com>
2024-12-08 15:30:04 +0300

    decimal done

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -168,6 +168,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		static WORD operation = 0;
 		static BOOL input = FALSE;
 		static BOOL operation_input = FALSE;
+		
 
 		SetFocus(hwnd);
 
@@ -179,7 +180,11 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
 		{
-			if (operation_input) SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"");
+			if (operation_input) 
+			{
+				SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"");
+				operation_input = FALSE;
+			}
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
 			
@@ -221,7 +226,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
 			if (input && a == DBL_MIN) a = atof(sz_display);
 			//input = FALSE;
-			if (operation) SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+			if (operation && input) SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
 			operation = LOWORD(wParam);
 			operation_input = TRUE;
 		}
